72f25060f6a735e8477a175962fb675b5f610414
Bruce Simiyu <bruce14simiyu@gmail.com>
2023-09-01 09:14:01 +0300

    Nyumbani Homes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./main-page/header";
+function App() {
+  return (
+    <>
+      <Header description="Owning a Home has Never Been Easier" />
+    </>
+  );
+}
+
+export default App;
