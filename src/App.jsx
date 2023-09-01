923e9576f97813df7a3cdc07697fff5ab9361bca
Bruce Simiyu <bruce14simiyu@gmail.com>
2023-09-01 17:47:27 +0300

    Testing workflows

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./main-page/header";
 function App() {
   return (
-    <>
+    <div className="bg-red-700 h-screen w-full">
       <Header description="Owning a Home has Never Been Easier" />
-    </>
+    </div>
   );
 }
 
